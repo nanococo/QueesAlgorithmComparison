d199b35485d56c01c41ee869a5c926b67b31f89f
Sebastian <salpizar@outcoding.com>
2020-06-07 19:08:52 -0600

    Added final commit with changes for randomized class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,21 @@
+import java.time.Duration;
+import java.time.Instant;
+
 public class Main {
     public static void main(String[] args) {
-        BacktrackingQueens backtrackingQueens = new BacktrackingQueens(20);
+        int size = 14;
+
+        Instant startsBackTracking = Instant.now();
+        BacktrackingQueens backtrackingQueens = new BacktrackingQueens(size);
+        Instant endsBacktracking = Instant.now();
+        System.out.println(Duration.between(startsBackTracking, endsBacktracking).toMillis());
+
+        System.out.println("----------------------------");
+
+
+        Instant startsRandomized = Instant.now();
+        RandomizedQueens randomizedQueens = new RandomizedQueens(size);
+        Instant endsRandomized = Instant.now();
+        System.out.println(Duration.between(startsRandomized, endsRandomized).toMillis());
     }
 }
--- src/main/java/RandomizedQueens.java
+++ src/main/java/RandomizedQueens.java
@@ -1,2 +1,165 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class RandomizedQueens {
+    private final int[][] board;
+    private final int boardSize;
+    private Random random = new Random();
+
+
+    public RandomizedQueens(int x) {
+        assert x!= 0;
+
+        this.board = new int[x][x];
+        this.boardSize = x;
+        boolean complete = false;
+
+        while (!complete){
+            complete = lasVegasQueens();
+        }
+
+        for(int i=0; i<board[0].length; i++){
+            StringBuilder line = new StringBuilder();
+            for(int j=0; j<board[0].length; j++){
+                line.append(board[i][j]);
+            }
+            System.out.println(line);
+        }
+    }
+
+    private boolean lasVegasQueens() {
+        List<Integer> integers = new ArrayList<Integer>();
+
+        for(int i=0; i<board[0].length; i++){
+            boolean triedCol;
+            do{
+                int j = random.nextInt(boardSize);
+                triedCol = integers.contains(j);
+                if(!triedCol){
+                    if(isSafe(i,j)){
+                        integers.add(j);
+                        board[i][j]=1;
+                    }
+                }
+            } while (triedCol);
+        }
+
+        if(isComplete()){
+           return true;
+        } else {
+            clear();
+            return false;
+        }
+    }
+
+    private void clear(){
+        for(int i=0; i<board[0].length; i++){
+            for(int j=0; j<board[0].length; j++){
+                board[i][j]=0;
+            }
+        }
+    }
+
+    private boolean isSafe(int i, int j){
+        if(rowFilled(i)){
+            return false;
+        }
+        if(columnFilled(j)){
+            return false;
+        }
+        if(diagonalsFilled(i,j)){
+            return false;
+        }
+        return true;
+    }
+
+
+    private boolean rowFilled(int row){
+        for(int cell=0; cell<boardSize; cell++){
+            if(board[row][cell]==1){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean columnFilled(int column){
+        for(int row=0; row<boardSize; row++){
+            if(board[row][column]==1){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean diagonalsFilled(int row, int column){
+        if(topLeftDiagonal(row, column)){
+            return true;
+        }
+        if(topRightDiagonal(row, column)){
+            return true;
+        }
+        if(bottomLeftDiagonal(row, column)){
+            return true;
+        }
+        if(bottomRightDiagonal(row, column)){
+            return true;
+        }
+        return false;
+    }
+
+    private boolean topLeftDiagonal(int row, int column) {
+        while (row>=0 && column>=0){
+            if(board[row][column]==1){
+                return true;
+            }
+            row--;
+            column--;
+        }
+        return false;
+    }
+
+    private boolean topRightDiagonal(int row, int column) {
+        while (row>=0 && column<boardSize){
+            if(board[row][column]==1){
+                return true;
+            }
+            row--;
+            column++;
+        }
+        return false;
+    }
+
+    private boolean bottomLeftDiagonal(int row, int column) {
+        while (row<boardSize && column>=0){
+            if(board[row][column]==1){
+                return true;
+            }
+            row++;
+            column--;
+        }
+        return false;
+    }
+
+    private boolean bottomRightDiagonal(int row, int column) {
+        while (row<boardSize && column<boardSize){
+            if(board[row][column]==1){
+                return true;
+            }
+            row++;
+            column++;
+        }
+        return false;
+    }
+
+    private boolean isComplete(){
+
+        for(int i=0; i<boardSize; i++){
+            if(!rowFilled(i)){
+                return false;
+            }
+        }
+        return true;
+    }
 }
